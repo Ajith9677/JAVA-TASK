be59f2dc4aa15302e5510c2ea14bd09b47b842e8
Ajith9677 <ajithksnew@gmail.com>
2025-10-30 21:16:46 +0530

    new code

---
+++ src/Collection/Class2.java
@@ -0,0 +1,18 @@
+package Collection;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Class2 {
+	public static void main(String[] args) {
+        Set<String> fruits = new HashSet<>();
+        fruits.add("Apple");
+        fruits.add("Banana");
+        fruits.add("Apple"); 
+
+        System.out.println("Fruits Set: " + fruits);
+        
+        System.out.println("alice".toLowerCase());
+        
+    }
+}
